0aff3797044544ba30217b75ff1b1d0eb8089a34
Shaikh Abdul <shaikhabdul2126@gmail.com>
2022-12-07 11:54:09 +0530

    color added

--- lib/color/appcolor.dart
+++ lib/color/appcolor.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 const Color navBarUnSelectedColor = Color(0xff979797);
 const Color navBarSelectedColor = Colors.black;
-const Color bottomSheetColor = Color(0xFFCC9900);
-
+Color bottomSheetColor = Colors.orange.shade50;
