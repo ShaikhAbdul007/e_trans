274039e2b16fef08917419683fd1807e61e7fba6
Shaikh Abdul <shaikhabdul2126@gmail.com>
2022-12-07 18:41:43 +0530

    fun added

--- lib/viewModel/bottom_navigation_view_model.dart
+++ lib/viewModel/bottom_navigation_view_model.dart
@@ -1,3 +1,9 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 
-class BottomNavigationViewModel extends GetxController {}
+class BottomNavigationViewModel extends GetxController {
+  RxInt currentIndex = 0.obs;
+
+  changeIndex(int index) {
+    currentIndex.value = index;
+  }
+}
