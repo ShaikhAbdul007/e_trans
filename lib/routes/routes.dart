af39cf38665d340b24a0e24bb165968deae05696
Shaikh Abdul <shaikhabdul2126@gmail.com>
2022-12-07 18:45:36 +0530

    route added

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,10 @@
 import 'package:get/get.dart';
+import 'package:trasn_human_resource_managment/view/attendance_page.dart';
+import 'package:trasn_human_resource_managment/view/bottom_navigation_view.dart';
 import 'package:trasn_human_resource_managment/view/forgetpassword_page.dart';
+import 'package:trasn_human_resource_managment/view/leave_page.dart';
 import 'package:trasn_human_resource_managment/view/login_page.dart';
+import 'package:trasn_human_resource_managment/view/setting_page.dart';
 import '../view/dashboard_page.dart';
 import '../view/splash_screen.dart';
 
@@ -10,5 +14,11 @@ class MyRoutes {
     GetPage(name: '/loginScreen', page: () => const LoginScreen()),
     GetPage(name: '/forgotScreen', page: () => const ForgotPasswordScreen()),
     GetPage(name: '/dashBoardScreen', page: () => const DashBoardPage()),
+    GetPage(name: '/attendanceScreen', page: () => const AttendanceScreen()),
+    GetPage(name: '/leaveScreen', page: () => const LeaveScreen()),
+    GetPage(name: '/proileScreen', page: () => const ProfileScreen()),
+    GetPage(
+        name: '/bottomNavigationScreen',
+        page: () => const CustomButtomNavigation()),
   ];
 }
