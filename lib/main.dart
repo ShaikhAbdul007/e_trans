743bbd0c091e1dff80ce63ae475288cc551af909
Shaikh Abdul <shaikhabdul2126@gmail.com>
2022-12-14 18:59:32 +0530

    initialRoute change

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'e-Trans',
           theme: CustomTheme.lightTheme(),
-          initialRoute: '/bottomNavigationScreen',
+          initialRoute: '/proileScreen',
           getPages: MyRoutes.viewPages,
         );
       },
