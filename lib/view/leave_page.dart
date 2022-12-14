c549379c11814ab5dde1bb9278dbd3f4ef9f8f4f
Shaikh Abdul <shaikhabdul2126@gmail.com>
2022-12-14 18:59:18 +0530

    app bar added

--- lib/view/leave_page.dart
+++ lib/view/leave_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trasn_human_resource_managment/view/drawer_page.dart';
 
 class LeaveScreen extends StatelessWidget {
   const LeaveScreen({super.key});
@@ -8,12 +10,17 @@ class LeaveScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(15.r),
+          bottomRight: Radius.circular(15.r),
+        )),
         title: Text(
           'Leave',
           style: Theme.of(context).textTheme.headline5,
         ),
       ),
-      drawer: Drawer(),
+      drawer: const CustomDrawer(),
     );
   }
 }
